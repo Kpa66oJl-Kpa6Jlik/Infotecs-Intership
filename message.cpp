4c0dcebbfd7bb48a8d36f6d40d30197d8de3379e
Вадим Максимов <vadmax333@yandex.ru>
2025-07-30 20:23:58 +0500

    Добавлены реализации методов класса сообщений

--- message.cpp
+++ message.cpp
@@ -1,3 +1,35 @@
 #include "message.h"
 
-Message::Message() {}
+Message::Message(std::string content, Importance importance)
+    : _text(content), _importance(importance), _time(std::time(0))
+{}
+
+Importance Message::getImportance() const
+{
+    return _importance;
+}
+
+std::string Message::getContent() const
+{
+    return _text;
+}
+
+time_t Message::getTime() const
+{
+    return _time;
+}
+
+std::string Message::getImportanceString() const
+{
+    switch (_importance){
+    case Importance::Low:
+        return "low";
+        break;
+    case Importance::Medium:
+        return "medium";
+        break;
+    case Importance::High:
+        return "high";
+        break;
+    }
+}
